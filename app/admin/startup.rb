c18d44d13e583b02335f16f6570e1871c6cf17f2
Paula Ibieta <paula.ibieta@gmail.com>
2016-05-31 20:54:59 -0700

    fixed active admin to allow image uploads and to show images

--- app/admin/startup.rb
+++ app/admin/startup.rb
@@ -1,6 +1,28 @@
 ActiveAdmin.register Startup do
 
-  permit_params :name, :description, :approved
+  permit_params :name, :description, :approved, :company_image
+
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.inputs "Startup", :multipart => true do
+      f.input :name
+      f.input :description
+      f.input :approved
+      f.input :company_image, hint: f.startup.company_image? ? image_tag(f.startup.company_image.url, height: '100') : content_tag(:span, "Upload JPG/PNG/GIF image")
+    end
+    f.actions 
+  end
+
+  show do |startup|
+    attributes_table do
+      row :name 
+      row :description
+      row :approved
+      row :company_image do
+        startup.company_image? ? image_tag(startup.company_image.url, height: '100') : content_tag(:span, "No photo yet")
+      end
+    end
+    active_admin_comments
+  end
 
 
 # See permitted parameters documentation:
