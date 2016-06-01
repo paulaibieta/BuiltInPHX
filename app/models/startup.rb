5cd93064ea4f5b7f757268b395cfb24c7f1d1ed9
Paula Ibieta <paula.ibieta@gmail.com>
2016-06-01 09:16:12 -0700

    made images responsive for index page with CSS

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,5 +1,5 @@
 class Startup < ActiveRecord::Base
-  has_attached_file :company_image, styles: { medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :company_image, styles: { medium: "400x250", thumb: "100x100#" }
   validates_attachment_content_type :company_image, content_type: /\Aimage\/.*\Z/
 
   scope :approved, -> { where(approved: true) }
