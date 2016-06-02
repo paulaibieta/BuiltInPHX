0995903c2003e9b9d2d1673854ad3f8bad3a53d0
Paula Ibieta <paula.ibieta@gmail.com>
2016-06-02 11:09:30 -0700

    fixed s3 url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,7 @@ Rails.application.configure do
 
   config.paperclip_defaults = {
   storage: :s3,
+  :s3_host_name => 's3-us-west-2.amazonaws.com',
   s3_credentials: {
     bucket: ENV["S3_BUCKET_NAME"],
     access_key_id: ENV["ACCESS_KEY_ID"],
